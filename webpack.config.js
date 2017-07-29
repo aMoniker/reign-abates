747fec7cccab18a8ca54fa21407ba97fc1ec2fcc
Jim Greenleaf <james.a.greenleaf@gmail.com>
2017-07-29 18:20:00 -0400

    set up webpack and browsersync

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      proxy: 'reignabates.app',
+      host: 'localhost',
+      port: 3000,
+    })
+  ]
+};
